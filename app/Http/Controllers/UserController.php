434cfa285867853d1ceee83342fd86103b96ed62
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2023-03-20 21:39:46 -0300

    login3

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if($user || Hash::check($request->password, $user->password)){
+            return $user;
+        }else{
+            return 'E-mail e/ou senha inválidos';
+        }
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,17 +3,18 @@
 <div class="container custom-login">
     <div class="row">
         <div class="col-sm-8 custom-login-2">
-            <form>
+            <form action="login" method="POST" id="login" name="login">
                 <div class="form-group">
+                    @csrf
                   <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
                 <div class="form-group">
                   <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                 </div>
                 <pre></pre>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Login</button>
               </form>
         </div>
     </div>
